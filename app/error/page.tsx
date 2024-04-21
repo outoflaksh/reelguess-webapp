06c53dcd035c95b0fb3a418d4be22c585b9aa718
Lakshya <lakshyam38@gmail.com>
2024-04-21 18:12:18 +0530

    Remove error message

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,9 +1,5 @@
-'use client'
 
-import { useSearchParams } from "next/navigation"
 
 export default function ErrorPage() {
-    const params = useSearchParams();
-    const errorMsg = params.get("msg")
-    return <p>Sorry, something went wrong {errorMsg}</p>
+    return <p>Sorry, something went wrong</p>
 }
